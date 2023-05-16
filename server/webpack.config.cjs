c7b6c8f0ebeccfa6b5cc702c265f28bd2155c34e
codeternalx123 <codeternal8@gmail.com>
2023-05-16 07:39:13 -0700

    updates

--- server/webpack.config.cjs
+++ server/webpack.config.cjs
@@ -8,6 +8,14 @@ module.exports = {
     publicPath: '/'
   },
   resolve: {
+    fallback: {
+      zlib: require.resolve('browserify-zlib'),
+      querystring: require.resolve('querystring-es3'),
+      path: require.resolve('path-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      fs: false,
+      stream: require.resolve('stream-browserify'),
+    },
     extensions: ['.js', '.jsx', '.json'],
   },
   mode: 'production'
